2c65af298984f0a5520b4c4e7146f1370d5cc5a7
Simon Edvardsson <Simon.Edvards@gmail.com>
2019-08-09 13:10:21 +0200

    Updates

---
+++ precache-manifest.62fe913c51c0e1872a4605b5e78bad69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "636716af8cfe2fa740e3a45b3899d8d8",
+    "url": "/website_multiscreen_view/index.html"
+  },
+  {
+    "revision": "72e0ef99f75b28c28162",
+    "url": "/website_multiscreen_view/static/css/main.35d13892.chunk.css"
+  },
+  {
+    "revision": "d783da2dcdf2f1660889",
+    "url": "/website_multiscreen_view/static/js/2.73d96024.chunk.js"
+  },
+  {
+    "revision": "72e0ef99f75b28c28162",
+    "url": "/website_multiscreen_view/static/js/main.15a22455.chunk.js"
+  },
+  {
+    "revision": "ce3345b37f6129ae5d49",
+    "url": "/website_multiscreen_view/static/js/runtime~main.1452770e.js"
+  }
+]);
